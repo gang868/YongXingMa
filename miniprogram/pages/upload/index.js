60d997c3602b62e85c3bf6034b4b9af21f6c3487
lingang <nmpxps@qq.com>
2021-12-06 21:56:36 +0800

    2021年12月6日家中完成图片上传功能和预览测试

--- miniprogram/pages/upload/index.js
+++ miniprogram/pages/upload/index.js
@@ -6,6 +6,8 @@ Page({
    */
   data: {
     files: [],
+    fileIds: [],
+    fileLinks: [],
     hasImage: false
   },
 
@@ -19,32 +21,74 @@ Page({
     })
   },
 
-  clearImages: function(e) {
+  clearImages: function (e) {
     this.setData({
       files: [],
       hasImage: false
     })
   },
 
-  chooseImage: function(){
+  chooseImage: function () {
     var that = this;
     wx.chooseImage({
-      count: 1,
+      count: 3,
       sizeType: ['original', 'compressed'],
       sourceType: ['album', 'camera'],
-      success: function(res) {
+      success: function (res) {
         that.setData({
-          files:that.data.files.concat(res.tempFilePaths),
+          files: that.data.files.concat(res.tempFilePaths),
           hasImage: true
         });
       }
     })
   },
 
-  previewImage: function(e) {
+  previewImage: function (e) {
     wx.previewImage({
       current: e.currentTarget.id,
       urls: this.data.files,
     })
+  },
+
+  uploadImages: function (e) {
+    wx.showLoading({
+      title: '正在上传资料....',
+    });
+    var i = 1,
+      successCount = 0;
+    var that = this;
+    this.setData({
+      fileIds: []
+    });
+    this.data.files.forEach(img => {
+      /**获取扩展名（不转换成小写云端居然不能预览） */
+      var ext = img.substring(img.lastIndexOf('.')).toLowerCase();
+      wx.cloud.uploadFile({
+        cloudPath: this.data.id + '_' + String(i) + ext,
+        filePath: img,
+        success: res => {
+          that.setData({
+            fileIds: that.data.fileIds.concat(res.fileID)
+          });
+          wx.cloud.getTempFileURL({
+            fileList: [res.fileID],
+            success: res => {
+              that.setData({
+                fileLinks: that.data.fileLinks.concat(res.fileList[0].tempFileURL)
+              });
+            }
+          })
+        }
+      });
+      i++;
+    });
+    wx.hideLoading();
+    console.log(this.data.fileIds);
+    wx.cloud.getTempFileURL({
+      fileList: this.data.fileIds,
+      success: res => {
+        console.log(res);
+      }
+    });
   }
 })
